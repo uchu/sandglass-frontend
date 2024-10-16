ea75d561e8a7a82a9e419917aeabf72f670cae04
liyunfei22 <liyunfei02@daojia-inc.com>
2024-10-16 18:59:47 +0800

    feat: Withdraw

--- frontend/src/components/web3/deposit-card.tsx
+++ frontend/src/components/web3/deposit-card.tsx
@@ -3,20 +3,19 @@
 import { FC, useState } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import { compactAddLength, hexToU8a, stringToU8a } from '@polkadot/util'
+import { useInkathon } from '@scio-labs/use-inkathon'
+import random from 'crypto-random-bigint'
+import { poseidon2 } from 'poseidon-bls12381'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
 import * as z from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Form, FormControl, FormItem, FormLabel } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { compactAddLength, hexToU8a, stringToU8a } from '@polkadot/util'
-import { useInkathon } from '@scio-labs/use-inkathon'
-import random from 'crypto-random-bigint'
-import { MerkleTree } from 'fixed-merkle-tree'
-import { poseidon2 } from "poseidon-bls12381"
-import toast from 'react-hot-toast'
-import { groth16 } from "snarkjs"
+
 import vkey from './verification_key.json'
 
 const formSchema = z.object({
@@ -31,105 +30,77 @@ export const DepositCard: FC = () => {
     resolver: zodResolver(formSchema),
   })
 
-  const { register, reset, handleSubmit, formState: { errors } } = form
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = form
   const { api, activeAccount, activeSigner } = useInkathon()
-  function toFixedHex(value: any, length = 32) {
-    const isBuffer = value instanceof Buffer
-  
-    const str = isBuffer ? value.toString('hex') : BigInt(value).toString(16)
-    return '0x' + str.padStart(length * 2, '0')
-  }
-// Update Greeting
-const deposit: SubmitHandler<z.infer<typeof formSchema>> = async ({ amount, faCode }) => {
-  console.log(amount, faCode)
-  if (!activeAccount || !activeSigner || !api) {
-    toast.error('Wallet not connected. Try again…')
-    return
-  }
-  const a2vkey = stringToU8a(JSON.stringify(vkey));
-  const compact_a2vkey = compactAddLength(a2vkey);
 
-  const t = await api.tx.mixer
-  .setupVerification(compact_a2vkey)
-  .signAndSend(activeAccount.address);
-  console.log(`Submitted with hash ${t}`);
-  const sk2 = BigInt( random(64) ); //random number
-  const cmt2 = poseidon2([sk2, BigInt(0)]);
-  console.log("@@@hash: ", cmt2.toString(16),  cmt2.toString());
-  console.log("@@@sk2: ", sk2);
-
-  const a = hexToU8a("0x" + cmt2.toString(16));
-  const compact_a = compactAddLength(a);
-
-  const txHash = await api.tx.mixer
-  .deposit(compact_a)
-  .signAndSend(activeAccount.address, { signer: activeSigner }, ({ status }) => {
-    if (status.isInBlock) {
-      console.log(`Completed at block hash #${status.asInBlock.toString()}`)
-    } else {
-      console.log(`Current status: ${status.type}`)
+  // Update Greeting
+  const deposit: SubmitHandler<z.infer<typeof formSchema>> = async ({ amount, faCode }) => {
+    console.log(amount, faCode)
+    if (!activeAccount || !activeSigner || !api) {
+      toast.error('Wallet not connected. Try again…')
+      return
     }
-  })
-  console.log(`Submitted with hash ${txHash}`);
+    const a2vkey = stringToU8a(JSON.stringify(vkey))
+    const compact_a2vkey = compactAddLength(a2vkey)
 
-  const hashFun = (left: any, right: any) => poseidon2([BigInt(left), BigInt(right)]).toString();
+    const t = await api.tx.mixer
+      .setupVerification(compact_a2vkey)
+      .signAndSend(activeAccount.address)
+    await delay(10000)
 
-  const tree = new MerkleTree(8, undefined, {
-    zeroElement: '0',
-    hashFunction: hashFun
-  });
+    console.log(`Submitted with hash ${t}`)
+    const sk2 = BigInt(random(64)) //random number
+    const cmt2 = poseidon2([sk2, BigInt(0)])
+    console.log('@@@hash: ', cmt2.toString(16), cmt2.toString())
+    console.log('@@@sk2: ', sk2)
 
-  const merkleCommitmentsVec = (await api.query.mixer.merkleVec()).toJSON();
-  const o = JSON.parse(JSON.stringify(merkleCommitmentsVec));
+    const a = hexToU8a('0x' + cmt2.toString(16))
+    const compact_a = compactAddLength(a)
 
-  for (const k in o) {
-    console.log(k, o[k]);
-    const cm = BigInt(o[k]);
-    tree.bulkInsert([cm.toString()])
+    const txHash = await api.tx.mixer
+      .deposit(compact_a)
+      .signAndSend(activeAccount.address, { signer: activeSigner }, ({ status }) => {
+        if (status.isInBlock) {
+          console.log(`Completed at block hash #${status.asInBlock.toString()}`)
+        } else {
+          console.log(`Current status: ${status.type}`)
+        }
+      })
+    await delay(10000)
+
+    downloadProof(sk2)
+    toast.success('deposit successfully!')
   }
 
-  // product root
-  const root = toFixedHex(tree.root)
-  console.log("@@@ local root3", root, tree.root.toString());
+  const downloadProof = (proof: any) => {
+    const blob = new Blob([proof], { type: 'application/text' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
 
-  console.log("@@@ tree is ", tree);
+    a.download = proof // 文件名
 
-  // product proof
-  const commitment = cmt2.toString();
-  const leafIndex = tree.indexOf(commitment);  
-  console.log( "leafIndex", leafIndex);
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    URL.revokeObjectURL(url) // 释放内存
+  }
 
-  const nullifier = poseidon2([BigInt(leafIndex), sk2, ]);
-
-  const { pathElements, pathIndices } = tree.path(leafIndex)
-
-  const input = {
-    root: tree.root.toString(),
-    nullifierHash: nullifier.toString(),
-    secret: sk2.toString(),
-    paths2_root: pathElements,
-    paths2_root_pos: pathIndices,
-  };
-
-  console.log("@@@input is, ",    JSON.stringify( input ) );
-
-  const {proof, publicSignals} = await groth16.fullProve(input, "./mixer.wasm", "./mixer_0001.zkey")
-
-  const a2nullifier = hexToU8a("0x" + nullifier.toString(16));
-  const compact_a2nullifier = compactAddLength(a2nullifier);
-
-  const a2root = hexToU8a(root);
-  const compact_a2root = compactAddLength(a2root);
-
-  console.log("@@@ proof is", JSON.stringify(proof));
-}
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 
   return (
     <div className="my-8 flex max-w-[220rem] grow flex-col gap-4">
       <Form {...form}>
         <Card>
           <CardHeader>
-            <h2 className="text-left text-primary font-sans font-bold text-2xl">Deposit</h2>
+            <h2 className="text-left font-sans text-2xl font-bold text-primary">Deposit</h2>
           </CardHeader>
           <CardContent className="pt-6">
             <form onSubmit={handleSubmit(deposit)} className="flex flex-col justify-end gap-2">
@@ -137,7 +108,11 @@ const deposit: SubmitHandler<z.infer<typeof formSchema>> = async ({ amount, faCo
                 <FormLabel className="text-base">Amount</FormLabel>
                 <FormControl>
                   <div className="flex gap-2">
-                    <Input type="number" disabled={form.formState.isSubmitting}  {...register('amount', { required: true })} />
+                    <Input
+                      type="number"
+                      disabled={form.formState.isSubmitting}
+                      {...register('amount', { required: true })}
+                    />
                   </div>
                 </FormControl>
               </FormItem>
@@ -152,7 +127,7 @@ const deposit: SubmitHandler<z.infer<typeof formSchema>> = async ({ amount, faCo
               <FormItem>
                 <FormControl>
                   <div className="flex justify-center p-4">
-                  <Button
+                    <Button
                       type="submit"
                       className="bg-primary font-bold"
                       disabled={fetchIsLoading || form.formState.isSubmitting}
--- frontend/src/components/web3/withdraw-card.tsx
+++ frontend/src/components/web3/withdraw-card.tsx
@@ -2,18 +2,24 @@
 
 import { FC, useState } from 'react'
 
-import { Button } from '@/components/ui/button'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { compactAddLength, hexToU8a, stringToU8a } from '@polkadot/util'
+import { useInkathon } from '@scio-labs/use-inkathon'
+import { MerkleTree } from 'fixed-merkle-tree'
+import { poseidon2 } from 'poseidon-bls12381'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+import { groth16 } from 'snarkjs'
 import * as z from 'zod'
 
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Form, FormControl, FormItem, FormLabel } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 
 const formSchema = z.object({
   note: z.string().min(1).max(90),
-  address: z.string().min(1).max(90)
+  address: z.string().min(1).max(90),
 })
 
 export const WithdrawCard: FC = () => {
@@ -23,16 +29,94 @@ export const WithdrawCard: FC = () => {
     resolver: zodResolver(formSchema),
   })
 
+  const { api, activeAccount, activeSigner } = useInkathon()
+
   const { register, reset, handleSubmit } = form
   const withdraw: SubmitHandler<z.infer<typeof formSchema>> = async ({ note, address }) => {
+    if (!activeAccount || !activeSigner || !api) {
+      toast.error('Wallet not connected. Try again…')
+      return
+    }
     console.log(note, address)
+    const sk2 = BigInt(note)
+    const cmt2 = poseidon2([sk2, BigInt(0)])
+    const hashFun = (left: any, right: any) => poseidon2([BigInt(left), BigInt(right)]).toString()
+
+    const tree = new MerkleTree(8, undefined, {
+      zeroElement: '0',
+      hashFunction: hashFun,
+    })
+
+    const merkleCommitmentsVec = (await api.query.mixer.merkleVec()).toJSON()
+    const o = JSON.parse(JSON.stringify(merkleCommitmentsVec))
+
+    for (const k in o) {
+      console.log(k, o[k])
+      const cm = BigInt(o[k])
+      tree.bulkInsert([cm.toString()])
+    }
+
+    // product root
+    const root = toFixedHex(tree.root)
+    console.log('@@@ local root3', root, tree.root.toString())
+
+    console.log('@@@ tree is ', tree)
+
+    // product proof
+    const commitment = cmt2.toString()
+    const leafIndex = tree.indexOf(commitment)
+    console.log('leafIndex', leafIndex)
+
+    const nullifier = poseidon2([BigInt(leafIndex), sk2])
+
+    const { pathElements, pathIndices } = tree.path(leafIndex)
+
+    const input = {
+      root: tree.root.toString(),
+      nullifierHash: nullifier.toString(),
+      secret: sk2.toString(),
+      paths2_root: pathElements,
+      paths2_root_pos: pathIndices,
+    }
+
+    console.log('@@@input is, ', JSON.stringify(input))
+
+    const { proof, publicSignals } = await groth16.fullProve(
+      input,
+      './mixer.wasm',
+      './mixer_0001.zkey',
+    )
+
+    const a2nullifier = hexToU8a('0x' + nullifier.toString(16))
+    const compact_a2nullifier = compactAddLength(a2nullifier)
+
+    const a2root = hexToU8a(root)
+    const compact_a2root = compactAddLength(a2root)
+
+    console.log('@@@ proof is', JSON.stringify(proof))
+    const a2proof = stringToU8a(JSON.stringify(proof))
+    const compact_a2proof = compactAddLength(a2proof)
+    const txHash2 = await api.tx.mixer
+      .withdraw(compact_a2proof, compact_a2root, compact_a2nullifier, address)
+      .signAndSend(activeAccount.address)
+    await delay(5000)
+    console.log(`withdraw with hash ${txHash2}`)
+  }
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+  function toFixedHex(value: any, length = 32) {
+    const isBuffer = value instanceof Buffer
+
+    const str = isBuffer ? value.toString('hex') : BigInt(value).toString(16)
+    return '0x' + str.padStart(length * 2, '0')
   }
   return (
     <div className="my-8 flex max-w-[220rem] grow flex-col gap-4">
       <Form {...form}>
         <Card>
           <CardHeader>
-            <h2 className="text-left text-primary font-sans font-bold text-2xl">Withdraw</h2>
+            <h2 className="text-left font-sans text-2xl font-bold text-primary">Withdraw</h2>
           </CardHeader>
           <CardContent className="pt-6">
             <form onSubmit={handleSubmit(withdraw)} className="flex flex-col justify-end gap-2">
@@ -40,7 +124,10 @@ export const WithdrawCard: FC = () => {
                 <FormLabel className="text-base">Note</FormLabel>
                 <FormControl>
                   <div className="flex gap-2">
-                    <Input disabled={form.formState.isSubmitting} {...register('note', {required: true})} />
+                    <Input
+                      disabled={form.formState.isSubmitting}
+                      {...register('note', { required: true })}
+                    />
                   </div>
                 </FormControl>
               </FormItem>
@@ -49,14 +136,17 @@ export const WithdrawCard: FC = () => {
                 <FormLabel className="text-base">recipient address</FormLabel>
                 <FormControl>
                   <div className="flex gap-2">
-                    <Input disabled={form.formState.isSubmitting} {...register('address', {required: true})} />
+                    <Input
+                      disabled={form.formState.isSubmitting}
+                      {...register('address', { required: true })}
+                    />
                   </div>
                 </FormControl>
               </FormItem>
               <FormItem>
                 <FormControl>
                   <div className="flex justify-center p-4">
-                  <Button
+                    <Button
                       type="submit"
                       className="bg-primary font-bold"
                       disabled={fetchIsLoading || form.formState.isSubmitting}
